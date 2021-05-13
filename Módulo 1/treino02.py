2f11f82fa7eb9d6fd8859d52d3b6b44f35aba544
Rodrigo Brust <rodrigobrusts@gmail.com>
2021-05-13 14:17:14 -0300

    updating content

--- Módulo 1/treino02.py
+++ Módulo 1/treino02.py
@@ -10,7 +10,7 @@ default_args = {
     'owner': 'Rodrigo - minerAI',
     'depends_on_past': False,
     'start_date': datetime(2021, 3, 16, 18),  # ano mes dia e hora
-    'email': ['rodrigobrusts@gmail.com', 'felipe.almeida@minerai.com.br'],
+    'email': ['rodrigobrusts@gmail.com'],
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
